f9ca9bf53937ac961951b67a38464596f799761d
torres10luis <Luis.angel_15@yahoo.com>
2018-06-20 15:42:05 -0700

    binding directives properties and passing in data

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, ElementRef, OnInit, HostListener, HostBinding } from '@angular/core';
+import { Directive, Renderer2, ElementRef, OnInit, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   // tslint:disable-next-line:directive-selector
@@ -6,13 +6,17 @@ import { Directive, Renderer2, ElementRef, OnInit, HostListener, HostBinding } f
 })
 export class BetterHighlightDirective implements OnInit {
 
+  @Input() defaultColor = 'transparent';
+ // tslint:disable-next-line:no-input-rename
+ @Input('betterHighlight') highlightColor = 'blue';
+
   // tslint:disable-next-line:no-shadowed-variable
   constructor(private renderer: Renderer2, private elementRef: ElementRef
   ) { }
 
 // @hostindingb
 
-  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+  @HostBinding('style.backgroundColor') backgroundColor = this.defaultColor;
 
   // bind to host property using hostBinding
   // define in string
@@ -22,17 +26,18 @@ export class BetterHighlightDirective implements OnInit {
   ngOnInit() {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
     // forth argument is optional, we can sert !important in that argument
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseEnter() { // hostlistener decorator is added to a method we want to execute on an event;
   // listening for mouseenter on host
 
-  this.backgroundColor = 'blue';
+  this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseOut() {
 
-    this.backgroundColor = 'red';
+    this.backgroundColor = this.defaultColor;
 
   }
 
